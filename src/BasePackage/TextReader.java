eaf3e842d44e18e3ff9fd21758b21099254270c6
LucasSalu <samuel_6000@outlook.com>
2020-10-26 21:13:00 -0300

    TextReader

--- src/BasePackage/TextReader.java
+++ src/BasePackage/TextReader.java
@@ -1,5 +1,28 @@
 package BasePackage;
 
-public class TextReader {
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public  class TextReader {
+
+
+	public static void leitor(String path) throws IOException {
+	    String aux = null;
+		BufferedReader buffRead = new BufferedReader(new FileReader(path));
+		String linha = "";
+		while (true) {
+			if (linha != null) {
+				//System.out.println(linha);
+				aux = aux + linha;
+
+			} else
+				break;
+			linha = buffRead.readLine();
+		}
+        aux.replace("\n", " ");
+		System.out.println(aux);
+		buffRead.close();
+	}
 
 }
